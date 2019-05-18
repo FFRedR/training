571be3088a56feae74685b659c0b21731afc5ed3
Tsvigun Evgeny <t1evgeny93@gmail.com>
2019-05-18 14:40:08 +0300

    up

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -3,8 +3,7 @@ import { Navigation as nv } from './navigation';
 export function MainApp() {
     vf.init();
     nv.init();
-    console.log('red')
-    return "main application initiated";
+    return "The main application is initiated";
 }
 
 
